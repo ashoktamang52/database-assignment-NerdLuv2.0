59961c2188d6c4d426a967b2b9eb5cd9efc182d0
Ashok Tamang <ashok.tamang@bison.howard.edu>
2017-03-18 14:31:43 -0400

    Create matches.php with input form

---
+++ matches.php
@@ -0,0 +1,12 @@
+<?php include("top.html"); ?>
+<div>
+	<div> Returning User: </div>
+	<form action="/matches-submit.php">
+		Name:
+		<input type="text" name="name" maxlength="16"><br>
+		<input type="submit" value="View My Matches">
+	</form>
+
+</div>
+
+<?php include("bottom.html"); ?>
