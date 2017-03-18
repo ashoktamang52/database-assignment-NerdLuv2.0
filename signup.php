6d52dab3e0ae7d2ac6e54fb6fd859b6c17e7b046
Ashok Tamang <ashok.tamang@bison.howard.edu>
2017-03-18 19:02:54 -0400

    Create signup page with signup form.

---
+++ signup.php
@@ -0,0 +1,26 @@
+<?php include("top.html"); ?>
+<div>
+	<div> Returning User: </div>
+	<form action="/matches-submit.php">
+		Name: <input type="text" name="name" size="17" maxlength="16"><br>
+        Gender:
+            <input type="radio" name="gender" value="M">Male
+            <input type="radio" name="gender" value="F" checked>Female<br>
+        Age: <input type="text" name="age" size="6" maxlength="2"><br>
+        Personality type: <input type="text" name="personality_type" size="6" maxlength="4"><br>
+        Favorite OS: 
+            <select name="os">
+                <option value="Windows">Windows</option>
+                <option value="Mac OS X">Mac OS X</option>
+                <option value="Linux">Linux</option>
+            </select><br>
+        Seeking age:
+            <input type="text" name="min_seek_age" size="6" maxlength="2" placeholder="min">
+            to
+            <input type="text" name="max_seek_age" size="6" maxlength="2" placeholder="max"><br>
+		<input type="submit" value="Sign Up">
+	</form>
+
+</div>
+
+<?php include("bottom.html"); ?>
