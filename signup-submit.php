d99c758ca8a9440e182329e2318adc14657776bf
Ashok Tamang <ashok.tamang@bison.howard.edu>
2017-03-19 18:31:28 -0400

    Validate signup form for name, and age.

--- signup-submit.php
+++ signup-submit.php
@@ -1,5 +1,25 @@
 <?php include("top.html"); ?>
 <?php
+// name should be letters only and every word should be started by upper
+// case letter
+// throw error if digits exist in the string.
+if (preg_match("/[0-9]/", $_POST["name"]) === 1) {
+    echo "Error";
+}
+// every unique word should start with upper case letter
+$name_list = explode(" ", $_POST["name"]);
+for ($i = 0; $i < count($name_list); $i++) {
+    if(strcmp(ucfirst($name_list[$i]),$name_list[$i]) !== 0) {
+        echo "Error";
+        break;
+    }
+}
+
+//validate age
+if (!is_numeric($_POST["age"]))
+    echo "Age is not a number";
+
+
 echo $_POST["name"];
 echo $_POST["gender"];
 echo $_POST["age"];
