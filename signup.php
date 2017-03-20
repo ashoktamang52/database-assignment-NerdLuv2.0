108738922e45279c43ed313c7a9224dfc7c42f19
Ashok Tamang <ashok.tamang@bison.howard.edu>
2017-03-20 13:47:37 -0400

    Add css effects for signup form.

--- signup.php
+++ signup.php
@@ -1,24 +1,30 @@
 <?php include("top.html"); ?>
 <div>
-	<div> Returning User: </div>
 	<form action="/signup-submit.php" method="post">
-		Name: <input type="text" name="name" size="17" maxlength="16"><br>
-        Gender:
+    <fieldset>
+        <legend> New User Signup: </legend>
+		<strong class="column">Name: </strong>
+        <input type="text" name="name" size="17" maxlength="16"><br>
+        <strong class="column"> Gender: </strong>
             <input type="radio" name="gender" value="M">Male
             <input type="radio" name="gender" value="F" checked>Female<br>
-        Age: <input type="text" name="age" size="6" maxlength="2"><br>
-        Personality type: <input type="text" name="personality_type" size="6" maxlength="4"><br>
-        Favorite OS: 
+        <strong class="column">Age: </strong>
+        <input type="text" name="age" size="6" maxlength="2"><br>
+        <strong class="column">Personality type: </strong>
+        <input type="text" name="personality_type" size="6" maxlength="4">
+        (<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a>) <br />
+        <strong class="column">Favorite OS: </strong>
             <select name="os">
                 <option value="Windows">Windows</option>
                 <option value="Mac OS X">Mac OS X</option>
                 <option value="Linux">Linux</option>
             </select><br>
-        Seeking age:
+        <strong class="column"> Seeking age: </strong>
             <input type="text" name="min_seek_age" size="6" maxlength="2" placeholder="min">
             to
             <input type="text" name="max_seek_age" size="6" maxlength="2" placeholder="max"><br>
 		<input type="submit" value="Sign Up">
+    </fieldset>
 	</form>
 
 </div>
