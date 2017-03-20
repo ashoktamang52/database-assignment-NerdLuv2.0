ec8d8f602feb2dfe7f878b6951b1203810993450
Ashok Tamang <ashok.tamang@bison.howard.edu>
2017-03-19 20:03:21 -0400

    Validate all user form info and parse all data into a one line string for append.

--- signup-submit.php
+++ signup-submit.php
@@ -3,21 +3,31 @@
 // name should be letters only and every word should be started by upper
 // case letter
 // throw error if digits exist in the string.
+
+// Bool to store any error during validation. 
+$any_error = FALSE;
+
+
 if (preg_match("/[0-9]/", $_POST["name"]) === 1) {
-    echo "Error";
+    echo "Error", PHP_EOL;
+    $any_error = TRUE;
+
 }
 // every unique word should start with upper case letter
 $name_list = explode(" ", $_POST["name"]);
 for ($i = 0; $i < count($name_list); $i++) {
     if(strcmp(ucfirst($name_list[$i]),$name_list[$i]) !== 0) {
         echo "Error";
+        $any_error = TRUE;
         break;
     }
 }
 
 //validate age
-if (!is_numeric($_POST["age"]))
-    echo "Age is not a number";
+if (!is_numeric($_POST["age"])) {
+    echo "Age is not a number.";
+    $any_error = TRUE;
+}
 
 //validate personality type
 $personality = array("ESTJ", "ISTJ", "ENTJ", "INTJ", 
@@ -27,10 +37,32 @@ $personality = array("ESTJ", "ISTJ", "ENTJ", "INTJ",
                 );
 if (!in_array($_POST["personality_type"], $personality)) {
     echo "Enter a valid Personality type";
+    $any_error = TRUE;
 }
-echo $_POST["personality_type"];
-echo $_POST["os"];
-echo $_POST["min_seek_age"];
-echo $_POST["max_seek_age"];
+
+// validate min/max seeking age.
+if (!is_numeric($_POST["min_seek_age"])) {
+    echo "Min seeking age is not a number.";
+    $any_error = TRUE;
+}
+
+if (!is_numeric($_POST["max_seek_age"])) {
+    echo "Max seeking age is not a number.";
+    $any_error = TRUE;
+}
+// Write to singles.txt after validation. 
+if (!$any_error) {
+    //parse form details into a one line
+    $user_details = array($_POST["name"],
+                        $_POST["gender"],
+                        $_POST["age"],
+                        $_POST["personality_type"],
+                        $_POST["os"],
+                        $_POST["min_seek_age"],
+                        $_POST["max_seek_age"]
+                    );
+    $user_info_to_write = implode(",", $user_details);
+    echo $user_info_to_write; 
+}
 ?>
 <?php include("bottom.html"); ?>
